0521ee8f9adb5bac39ce743dcced861fb413bcf8
skriLLeX123 <bhushan.navgire@gmail.com>
2019-01-06 21:07:09 -0500

    Day 1

--- Python Programs/join method.py
+++ Python Programs/join method.py
@@ -1,8 +1,9 @@
-
+#program to implement join method functionality
 if __name__ == '__main__':
     n = int(input("Enter Number: "))
 
 list1=[str(x) for x in range(1,n+1)]
-#join method joins the elements of iterable specifed in single quotation
+
+#join method joins the elements of iterable with charachter specifed in single quotation
 print(''.join(list1))
     
