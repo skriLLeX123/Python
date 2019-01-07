f32720cde13ecc69c24ac06f047e66aa73f7aaad
skriLLeX123 <bhushan.navgire@gmail.com>
2019-01-07 00:52:57 -0500

    Day 2

---
+++ Python Programs/Hash function.py
@@ -0,0 +1,8 @@
+#program to implement functionality of hash function
+#The hash() method returns the hash value of an object if it has one.
+if __name__=="__main__":
+    n=int(input())
+    tuple1=tuple(map(int,input().split()))
+    print(hash(tuple1))
+
+    
