82bbd8d0c3f475f49ff49777fabd5aa012666dd2
skriLLeX123 <32133489+skriLLeX123@users.noreply.github.com>
2019-01-08 23:18:15 -0500

    Update Numpydotproduct.py

--- Python Numpy/Numpydotproduct.py
+++ Python Numpy/Numpydotproduct.py
@@ -3,7 +3,6 @@ import numpy
 c=int(input())
 a=numpy.array([[int(x) for x in input().split()]for i in range(c)])
 b=numpy.array([[int(x) for x in input().split()]for i in range(c)])
-
 print(numpy.dot(a,b))
 
 
