0efc9c6c8591e563f8420967d2f4f7f4b4b681d2
skriLLeX123 <bhushan.navgire@gmail.com>
2019-01-08 23:08:28 -0500

    ...

---
+++ Python Numpy/Numpydotproduct.py
@@ -0,0 +1,7 @@
+#program to demonstrate arithmetic operations on  umpy arrays
+import numpy
+c=int(input())
+a=numpy.array([[int(x) for x in input().split()]for i in range(c)])
+b=numpy.array([[int(x) for x in input().split()]for i in range(cprint(numpy.dot(a,b))
+
+
