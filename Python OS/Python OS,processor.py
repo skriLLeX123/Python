897859c0af7af9329d266ef0f307304b8af34d77
skriLLeX123 <bhushan.navgire@gmail.com>
2019-01-10 18:04:22 -0500

    ...

---
+++ Python OS/Python OS,processor.py
@@ -0,0 +1,6 @@
+from platform import *
+print(platform(0,1)) # returns the name of OS running python
+print(machine()) #returns the generic name of processor
+print(system()) #returns the generic name of OS running python
+print(version()) #returns the versio of OS
+print(processor()) #returns the full flexed name of the processor
